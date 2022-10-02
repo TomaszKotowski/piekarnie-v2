51e02872e07ff6b86876576ed5e41a33d1019f4e
Tomasz Kotowski <t.kotowski.2@gmail.com>
2022-10-02 20:43:41 +0200

    add new data, small fixes to ui

--- src/data/data.ts
+++ src/data/data.ts
@@ -5,6 +5,7 @@ type BakeryData = {
     instagram: string;
     website: string;
   };
+  location?: string;
 };
 
 export const bakeriesData: BakeryData[] = [
@@ -15,6 +16,8 @@ export const bakeriesData: BakeryData[] = [
       instagram: "https://www.instagram.com/swiezo.upieczona.piekarnia/",
       website: "https://swiezo-upieczona.com.pl/",
     },
+    location:
+      "https://www.google.pl/maps/place/%C5%9Awie%C5%BCo+Upieczona+Rodzinna+Piekarnia+Rzemie%C5%9Blnicza/@50.062405,19.9683293,17z/data=!4m5!3m4!1s0x47165bdbd241853b:0x8a60346bc4e85139!8m2!3d50.062405!4d19.970518",
   },
   {
     name: "Zaczyn",
@@ -22,6 +25,14 @@ export const bakeriesData: BakeryData[] = [
       facebook: "https://www.facebook.com/piekarnia.zaczyn",
       instagram: "https://www.instagram.com/zaczyn.piekarnia/",
       website: "https://piekarnia-zaczyn.pl/",
+    },
+  },
+  {
+    name: "Breaking Bread",
+    links: {
+      facebook: "https://www.facebook.com/breakingbreadkrakow",
+      instagram: "https://www.instagram.com/breakingbreadkrakow/",
+      website: "http://www.breakingbread.pl/",
     },
   },
 ];
