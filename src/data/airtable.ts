baf2af58a705a285469ee015a7e09f63fddcacb6
Tomasz Kotowski <t.kotowski.2@gmail.com>
2022-10-07 10:02:01 +0200

    move airtable file to typescript

---
+++ src/data/airtable.ts
@@ -0,0 +1,25 @@
+import { base, configure } from "airtable";
+
+type Fields = {
+  name: string;
+  facebook: string;
+  instagram: string;
+  website: string;
+};
+
+configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: import.meta.env.AIRTABLE_API_KEY,
+});
+
+const allBakeries = async () =>
+  base(import.meta.env.AIRTABLE_BASE_ID)
+    .table("Piekarnie")
+    .select({
+      view: "Grid view",
+      sort: [{ field: "name", direction: "asc" }],
+    })
+    .all()
+    .then((data) => data.map((records) => records.fields as Fields));
+
+export { base, allBakeries };
